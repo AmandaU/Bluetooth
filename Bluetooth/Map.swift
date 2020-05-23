1a65daa35e22c16d502a56c9fd6e892c7564664a
Amanda Baret <amanda@rapidfire.digital>
2020-05-23 13:48:46 +0200

    added map

---
+++ Bluetooth/Map.swift
@@ -0,0 +1,13 @@
+//
+//  Mao.swift
+//  Bluetooth
+//
+//  Created by Amanda Baret on 2020/05/23.
+//  Copyright © 2020 Amanda Baret. All rights reserved.
+//
+
+import UIKit
+
+class Mao: NSObject {
+
+}
---
+++ Bluetooth/Models/GadgetModel.swift
@@ -0,0 +1,13 @@
+//
+//  GadgetModel.swift
+//  Bluetooth
+//
+//  Created by Amanda Baret on 2020/05/23.
+//  Copyright © 2020 Amanda Baret. All rights reserved.
+//
+
+import UIKit
+
+class GadgetModel: NSObject {
+
+}
